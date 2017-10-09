001061492f4537685f7809af808287d213fd44db
Carson <cbrown@signalpath.com>
2017-10-09 16:26:50 -0400

    additions to example

--- app/routes.js
+++ app/routes.js
@@ -20,6 +20,11 @@ module.exports = function(app, passport) {
 
     // process the login form
     // app.post('/login', do all our passport stuff here);
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect :'/profile',
+        failureRedirect :'/login',
+        failureFlash : true
+    }));
 
     // =====================================
     // SIGNUP ==============================
@@ -33,6 +38,12 @@ module.exports = function(app, passport) {
 
     // process the signup form
     // app.post('/signup', do all our passport stuff here);
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect :'/profile',
+        failureRedirect :'/signup',
+        failureFlash : true
+    }))
+
 
     // =====================================
     // PROFILE SECTION =====================
